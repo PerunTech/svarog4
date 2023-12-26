b50f5571298c06fb9b797622045f2f8b8656b429
r.pejov <r.pejov@prtech.mk>
2023-12-26 10:08:01 +0100

    update test to clean up

--- tests/com/prtech/svarog/CodeListTest.java
+++ tests/com/prtech/svarog/CodeListTest.java
@@ -10,12 +10,16 @@ public class CodeListTest {
 
 	@Test
 	public void getCodeListMap() throws SvException {
-		try {
-			CodeList cl = new CodeList();
+		try(CodeList cl = new CodeList()) {
+			
 			HashMap<String, Long> hm = cl.getCodeListValues(Sv.ROOT_CODELIST);
-			System.out.println(hm.toString());
-			hm = cl.getCodeListValues(hm.get("ORG_UNIT_TYPE"));
-			System.out.println(hm.toString());
+			if (hm.containsKey("ORG_UNIT_TYPE")) {
+				hm = cl.getCodeListValues(hm.get("ORG_UNIT_TYPE"));
+				if (hm.size() < 1)
+					fail("codelists fail");
+
+			} else
+				fail("codelists fail");
 		} catch (Exception e) {
 			fail("failed codelists test");
 			// TODO: handle exception
